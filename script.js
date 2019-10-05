ffdd217dd028c0243bc4ad35fb76d9b5eb0f7530
Justin Detjen <jdetjen@gmx.com>
2019-10-05 15:45:00 -0500

    Re-add json files and refine eventListener

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const hamburger = document.querySelector(".fa-fas");
 const hiddenUL = document.querySelector("ul");
 
 hamburger.addEventListener("click", () => {
-hiddenUL.classList.toggle("main");
+hiddenUL.classList.toggle(".main");
 });
