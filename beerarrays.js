3ba703dfa1366cbd1bc81144b68713d1f3d9f0a3
Justin Detjen <jdetjen@gmx.com>
2019-10-05 18:07:22 -0500

    Add js nested variables and remove excess choices

---
+++ beerarrays.js
@@ -0,0 +1,66 @@
+const brewery1 = "Schlafly";
+const brewery2 = "Urban Chestnut";
+const brewery3 = "4 Hands";
+const brewery4 = "Civil Life";
+const brewery5 = "Modern";
+const brewery6 = "2nd Shift";
+const brewery7 = "Square One";
+
+const beer = {
+  brewery1: {
+    Pumpkin: "Pumpkin Ale",
+    Marzen: "Oktoberfest",
+    SummerAle: "Summer Lager",
+    WinterLager: "ESB",
+    WinterLager: "Christmas Ale",
+    WinterLager: "Merry Berry Ale",
+    IrishStout: "Irish-Style Extra Stout",
+    CoffeeStout: "Coffee Stout"
+  },
+  brewery2: {
+    Pumpkin: "Count Orlok Black Pumpkin Ale",
+    Marzen: "Oachkatzlschwoaf",
+    SummerAle: "Solaris",
+    SummerAle: "Big Shark Lemon Radler",
+    SummerAle: "Big Shark Grapefruit Radler",
+    WinterLager: "Horchata Kinsale",
+    WinterLager: "Cherry Thrale's",
+    IrishStout: "O'Florian's",
+    CoffeeStout: "Kinsale"
+  },
+  brewery3: {
+    Pumpkin: "Ill Repute",
+    SummerAle: "Dakine",
+    SummerAle: "On Cue",
+    SummerAle: "Contact High Juiced",
+    CoffeeStout: "Devil's Invention",
+    WinterLager: "Chocolate Milk Stout"
+  },
+  brewery4: {
+    Marzen: "Oktoberfest",
+    SummerAle: "Vienna Lager",
+    IrishStout: "Extra Stout",
+  },
+  brewery5: {
+    WinterLager: "Arkham's Finest",
+    WinterLager: "The Heeler",
+    IrishStout: "Clandestine",
+    Marzen: "Oktoberfest"
+  },
+  brewery6: {
+    Marzen: "Oktoberfest",
+    SummerAle: "Kolsch",
+    SummerAle: "Arancia E Ibisco",
+    WinterLager: "Cat Spit Stout",
+    WinterLager: "Liquid Spiritual Delight"
+  },
+  brewery7: {
+    Pumpkin: "Spiced Pumpkin Ale",
+    IrishStout: "Irish Stout",
+    Marzen: "Oktoberfest",
+    SummerAle: "Summer Saison",
+    WinterLager: "Smoked Porter"
+  }
+}
+
+const beerArr = [brewery1, brewery2, brewery3, brewery4, brewery5, brewery6, brewery7];
