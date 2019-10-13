b6d4bfd83296d6a3c040cd9460fd38e11935fcb4
Justin Detjen <jdetjen@gmx.com>
2019-10-13 15:42:56 -0500

    Fix array format for breweries and beer

--- beerarrays.js
+++ beerarrays.js
@@ -1,66 +1,43 @@
-const brewery1 = "Schlafly";
-const brewery2 = "Urban Chestnut";
-const brewery3 = "4 Hands";
-const brewery4 = "Civil Life";
-const brewery5 = "Modern";
-const brewery6 = "2nd Shift";
-const brewery7 = "Square One";
 
-const beer = {
-  brewery1: {
-    Pumpkin: "Pumpkin Ale",
-    Marzen: "Oktoberfest",
-    SummerAle: "Summer Lager",
-    WinterLager: "ESB",
-    WinterLager: "Christmas Ale",
-    WinterLager: "Merry Berry Ale",
-    IrishStout: "Irish-Style Extra Stout",
-    CoffeeStout: "Coffee Stout"
-  },
-  brewery2: {
-    Pumpkin: "Count Orlok Black Pumpkin Ale",
-    Marzen: "Oachkatzlschwoaf",
-    SummerAle: "Solaris",
-    SummerAle: "Big Shark Lemon Radler",
-    SummerAle: "Big Shark Grapefruit Radler",
-    WinterLager: "Horchata Kinsale",
-    WinterLager: "Cherry Thrale's",
-    IrishStout: "O'Florian's",
-    CoffeeStout: "Kinsale"
-  },
-  brewery3: {
-    Pumpkin: "Ill Repute",
-    SummerAle: "Dakine",
-    SummerAle: "On Cue",
-    SummerAle: "Contact High Juiced",
-    CoffeeStout: "Devil's Invention",
-    WinterLager: "Chocolate Milk Stout"
-  },
-  brewery4: {
-    Marzen: "Oktoberfest",
-    SummerAle: "Vienna Lager",
-    IrishStout: "Extra Stout",
-  },
-  brewery5: {
-    WinterLager: "Arkham's Finest",
-    WinterLager: "The Heeler",
-    IrishStout: "Clandestine",
-    Marzen: "Oktoberfest"
-  },
-  brewery6: {
-    Marzen: "Oktoberfest",
-    SummerAle: "Kolsch",
-    SummerAle: "Arancia E Ibisco",
-    WinterLager: "Cat Spit Stout",
-    WinterLager: "Liquid Spiritual Delight"
-  },
-  brewery7: {
-    Pumpkin: "Spiced Pumpkin Ale",
-    IrishStout: "Irish Stout",
-    Marzen: "Oktoberfest",
-    SummerAle: "Summer Saison",
-    WinterLager: "Smoked Porter"
-  }
-}
-
-const beerArr = [brewery1, brewery2, brewery3, brewery4, brewery5, brewery6, brewery7];
+const beer = [
+  { brewery: "Schlafly", Pumpkin: "Pumpkin Ale" },
+  { brewery: "Schlafly", Marzen: "Oktoberfest" },
+  { brewery: "Schlafly", SummerAle: "Summer Lager" },
+  { brewery: "Schlafly", WinterLager: "ESB" },
+  { brewery: "Schlafly", WinterLager: "Christmas Ale" },
+  { brewery: "Schlafly", WinterLager: "Merry Berry Ale" },
+  { brewery: "Schlafly", IrishStout: "Irish-Style Extra Stout" },
+  { brewery: "Schlafly", CoffeeStout: "Coffee Stout" },
+  { brewery: "Urban Chestnut", Pumpkin: "Count Orlok Black Pumpkin Ale" },
+  { brewery: "Urban Chestnut", Marzen: "Oachkatzlschwoaf" },
+  { brewery: "Urban Chestnut", SummerAle: "Solaris" },
+  { brewery: "Urban Chestnut", SummerAle: "Big Shark Lemon Radler" },
+  { brewery: "Urban Chestnut", SummerAle: "Big Shark Grapefruit Radler" },
+  { brewery: "Urban Chestnut", WinterLager: "Horchata Kinsale" },
+  { brewery: "Urban Chestnut", WinterLager: "Cherry Thrale's" },
+  { brewery: "Urban Chestnut", IrishStout: "O'Florian's" },
+  { brewery: "Urban Chestnut", CoffeeStout: "Kinsale" },
+  { brewery: "4 Hands", Pumpkin: "Ill Repute" },
+  { brewery: "4 Hands", SummerAle: "Dakine" },
+  { brewery: "4 Hands", SummerAle: "On Cue" },
+  { brewery: "4 Hands", SummerAle: "Contact High Juiced" },
+  { brewery: "4 Hands", CoffeeStout: "Devil's Invention" },
+  { brewery: "4 Hands", WinterLager: "Chocolate Milk Stout" },
+  { brewery: "Civil Life", Marzen: "Oktoberfest" },
+  { brewery: "Civil Life", SummerAle: "Vienna Lager" },
+  { brewery: "Civil Life", IrishStout: "Extra Stout" },
+  { brewery: "Modern", WinterLager: "Arkham's Finest" },
+  { brewery: "Modern", WinterLager: "The Heeler" },
+  { brewery: "Modern", IrishStout: "Clandestine" },
+  { brewery: "Modern", Marzen: "Oktoberfest" },
+  { brewery: "2nd Shift", Marzen: "Oktoberfest" },
+  { brewery: "2nd Shift", SummerAle: "Kolsch" },
+  { brewery: "2nd Shift", SummerAle: "Arancia E Ibisco" },
+  { brewery: "2nd Shift", WinterLager: "Cat Spit Stout" },
+  { brewery: "2nd Shift", WinterLager: "Liquid Spiritual Delight" },
+  { brewery: "Square One", Pumpkin: "Spiced Pumpkin Ale" },
+  { brewery: "Square One", IrishStout: "Irish Stout" },
+  { brewery: "Square One", Marzen: "Oktoberfest" },
+  { brewery: "Square One", SummerAle: "Summer Saison" },
+  { brewery: "Square One", WinterLager: "Smoked Porter" }
+]
