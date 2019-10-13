9fc9760141e281670d25eddafb83738b73e440af
Justin Detjen <jdetjen@gmx.com>
2019-10-13 17:57:49 -0500

    Add age verif enter button JS functionality

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ hiddenUL.classList.toggle(".main");
 });
 
 // Age Verif Card Info below
-
+//const made if necessary for later
 const flexDiv = document.querySelector("#flex-div");
 const ageVerifCard = document.querySelector(".age-verif-card");
 const label = document.querySelector(".label");
@@ -23,7 +23,7 @@ const enter = document.querySelector("#enter");
 // setTimeout(() => open(over21), 2000);
 
 function over21(Question, Choose, Yes, No, Enter) {
-  return `<div class="age-verif-card">
+  return `<div class="age-card">
        <label for="age-verif" class="label">${Question}</label>
       <select name="verif" id="age-select">
         <option id="blank" value="">${Choose}</option>
@@ -41,4 +41,27 @@ function over21(Question, Choose, Yes, No, Enter) {
 window.onload = function() {
   flexDiv.innerHTML += over21("Are you over 21?", "--Choose--", "Yes", "No", "Enter");
 }
+
+//Enter button functionality below
+enter.addEventListener("click", function() {
+  if (document.getElementById("age-select") === no) {
+    return ageDenied.innerHTML += tooYoung("Sorry, you are too young for this page.")
+  } else {
+    return document.prompt("Have a lot of fun!")
+  }
+})
+
+//Age Denied card below
+
+const ageDenied = document.querySelector("#card-age-denied");
+const sorry = document.querySelector("#sorry");
+
+function tooYoung(sorry) {
+  return `<div class="age-card">
+    <h3 id="sorry">${sorry}</h3>
+  </div>`
+}
+
+// ageDenied.innerHTML += tooYoung("Sorry, you are too young for this page.")
+
 
