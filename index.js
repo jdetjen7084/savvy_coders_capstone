ea6e8eb44e0c746262841800fb72b390473b2ea3
Justin Detjen <jdetjen@gmx.com>
2019-10-23 14:21:49 -0500

    Add router to index

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 import { Nav, Main, Footer } from "./components";
 import * as state from "./store";
 
+import Navigo from "navigo";
+import axios from "axios";
+import { capitalize } from "lodash";
+
+const router = new Navigo(location.origin);
+
 function render(st = state.Home) {
   document.querySelector("#root").innerHTML = `
     ${Nav()}
     ${Main(st)}
     ${Footer()}
   `;
+  router.updatePageLinks();
 }
+
+router
+  .on(":page", params =>
+  render(
+    state[
+    capitalize(params.page)]
+  )
+  )
+  .on("/", () => render())
+  .resolve();
+
+
+  // BELOW IS FROM IN CLASS PROJECT TO IMPORT BLOG POSTS
+
+  // axios
+  //   .get("https://jsonplaceholder.typicode.com/posts")
+  //   .then(response => {
+  //     state.Blog.main = response.data.map(
+  //       ({title, body}) => `
+  //       <article>
+  //         <h2>${title}</h2>
+  //         <p>${body}</p>
+  //       </article>
+  //     `
+  //     ).join("");
+  //     if (router.lastRouteResolved().params && capitalize(router.lastRouteResolved().params.page === "Blog")){
+  //       render(state.Blog);
+  //     }
+  //     console.log(router.lastRouteResolved());
+  //   })
+  //   .catch(err => console.log(err));
