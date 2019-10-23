f409546f2ff56b0b34e418a241630859720f86c6
Justin Detjen <jdetjen@gmx.com>
2019-10-23 14:00:12 -0500

    Add Links functionality to Nav

---
+++ components/Nav.js
@@ -0,0 +1,14 @@
+import { Links } from "../store"
+
+function linksBuilder(links) {
+  return links.map(link => `<li><a href="./${link}" data-navigo>${link}</a></li>`).join(" ");
+}
+
+export default () =>
+  `<nav>
+      <span class="fas fa-bars is-hidden--desktop"></span>
+      <ul class="is-hidden--mobile is-hidden--tablet is-shown--desktop">
+      ${linksBuilder(Links)}
+      </ul>
+    </nav>
+`;
