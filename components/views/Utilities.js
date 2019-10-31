6ec2ff51ec686bbd2d4023cfa39b507d26206a9f
Justin Detjen <jdetjen@gmx.com>
2019-10-30 20:41:21 -0500

    Create Utilities file in views

---
+++ components/views/Utilities.js
@@ -0,0 +1,12 @@
+export default () => `
+
+
+`
+
+
+// //Search button
+// const button = document.getElementById("search");
+
+// button.addEventListener("click", function(){
+//   console.log("This button is clicked!")
+// })
