9ac82b4484574c5d82c9134222dbc9176d835603
Justin Detjen <jdetjen@gmx.com>
2019-11-05 18:07:34 -0600

    Add properly cased index file.

---
+++ store/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home";
+export { default as Contact } from "./Contact";
+export { default as List } from "./List";
+export { default as Links } from "./Links";
+export { default as BeerData } from "./BeerData";
